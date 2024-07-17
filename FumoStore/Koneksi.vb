890112886b5816eca9032c6501a40a0fddf922ea
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-07-18 03:48:34 +0700

    add koneksi

---
+++ FumoStore/Koneksi.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Data
+
+Imports MySql.Data
+Imports MySql.Data.MySqlClient
+Imports Mysqlx
+
+Public Class Koneksi
+    Public Shared m_connStr As String = "server=localhost;user=root;database=aruskas;port=3306;password=;"
+    Public Shared m_conn As New MySqlConnection(m_connStr)
+
+    Public Shared Function Konek() As Boolean
+        Try
+            m_conn.Open()
+            MessageBox.Show("Koneksi Berhasil")
+            Return True
+        Catch ex As MySqlException
+            MessageBox.Show("Error: " & ex.Message)
+            Return False
+        End Try
+    End Function
+
+    Public Shared Function GetKoneksi() As MySqlConnection
+        Return m_conn
+    End Function
+
+    Public Shared Function Query(queryString As String) As MySqlCommand
+        Return New MySqlCommand(queryString, Koneksi.GetKoneksi())
+    End Function
+End Class
