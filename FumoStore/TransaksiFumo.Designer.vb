6dddcfc07af5108d210803a3542df3d0099b4e64
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-07-27 20:48:55 +0700

    📦 add tambah and hapus on transaksi

--- FumoStore/TransaksiFumo.Designer.vb
+++ FumoStore/TransaksiFumo.Designer.vb
@@ -24,26 +24,26 @@ Partial Class TransaksiFumo
     Private Sub InitializeComponent()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.KembaliToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.InHarga = New System.Windows.Forms.TextBox()
+        Me.BtnHapus = New System.Windows.Forms.Button()
+        Me.BtnEdit = New System.Windows.Forms.Button()
+        Me.BtnTambah = New System.Windows.Forms.Button()
+        Me.TableTransaksi = New System.Windows.Forms.DataGridView()
+        Me.InJumlah = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.fumoComboBox = New System.Windows.Forms.ComboBox()
+        Me.TotalHarga = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TotalHargaTampil = New System.Windows.Forms.TextBox()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.Label5 = New System.Windows.Forms.Label()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Kembalian = New System.Windows.Forms.TextBox()
         Me.Label6 = New System.Windows.Forms.Label()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.Button5 = New System.Windows.Forms.Button()
+        Me.BtnCetak = New System.Windows.Forms.Button()
+        Me.BtnBersih = New System.Windows.Forms.Button()
         Me.MenuStrip1.SuspendLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TableTransaksi, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'MenuStrip1
@@ -51,7 +51,7 @@ Partial Class TransaksiFumo
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.KembaliToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(423, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(651, 24)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -61,47 +61,47 @@ Partial Class TransaksiFumo
         Me.KembaliToolStripMenuItem.Size = New System.Drawing.Size(62, 20)
         Me.KembaliToolStripMenuItem.Text = "Kembali"
         '
-        'Button3
+        'BtnHapus
         '
-        Me.Button3.Location = New System.Drawing.Point(252, 132)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(76, 27)
-        Me.Button3.TabIndex = 16
-        Me.Button3.Text = "Hapus"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.BtnHapus.Location = New System.Drawing.Point(288, 132)
+        Me.BtnHapus.Name = "BtnHapus"
+        Me.BtnHapus.Size = New System.Drawing.Size(76, 27)
+        Me.BtnHapus.TabIndex = 16
+        Me.BtnHapus.Text = "Hapus"
+        Me.BtnHapus.UseVisualStyleBackColor = True
         '
-        'Button2
+        'BtnEdit
         '
-        Me.Button2.Location = New System.Drawing.Point(170, 132)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(76, 27)
-        Me.Button2.TabIndex = 15
-        Me.Button2.Text = "Edit"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.BtnEdit.Location = New System.Drawing.Point(206, 132)
+        Me.BtnEdit.Name = "BtnEdit"
+        Me.BtnEdit.Size = New System.Drawing.Size(76, 27)
+        Me.BtnEdit.TabIndex = 15
+        Me.BtnEdit.Text = "Edit"
+        Me.BtnEdit.UseVisualStyleBackColor = True
         '
-        'Button1
+        'BtnTambah
         '
-        Me.Button1.Location = New System.Drawing.Point(88, 132)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(76, 27)
-        Me.Button1.TabIndex = 14
-        Me.Button1.Text = "Tambah"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.BtnTambah.Location = New System.Drawing.Point(124, 132)
+        Me.BtnTambah.Name = "BtnTambah"
+        Me.BtnTambah.Size = New System.Drawing.Size(76, 27)
+        Me.BtnTambah.TabIndex = 14
+        Me.BtnTambah.Text = "Tambah"
+        Me.BtnTambah.UseVisualStyleBackColor = True
         '
-        'DataGridView1
+        'TableTransaksi
         '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(12, 165)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(398, 267)
-        Me.DataGridView1.TabIndex = 13
+        Me.TableTransaksi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.TableTransaksi.Location = New System.Drawing.Point(12, 165)
+        Me.TableTransaksi.Name = "TableTransaksi"
+        Me.TableTransaksi.Size = New System.Drawing.Size(627, 267)
+        Me.TableTransaksi.TabIndex = 13
         '
-        'InHarga
+        'InJumlah
         '
-        Me.InHarga.Location = New System.Drawing.Point(124, 65)
-        Me.InHarga.Name = "InHarga"
-        Me.InHarga.Size = New System.Drawing.Size(286, 20)
-        Me.InHarga.TabIndex = 12
+        Me.InJumlah.Location = New System.Drawing.Point(124, 65)
+        Me.InJumlah.Name = "InJumlah"
+        Me.InJumlah.Size = New System.Drawing.Size(515, 20)
+        Me.InJumlah.TabIndex = 12
         '
         'Label2
         '
@@ -123,21 +123,22 @@ Partial Class TransaksiFumo
         Me.Label1.TabIndex = 9
         Me.Label1.Text = "Fumo"
         '
-        'ComboBox1
+        'fumoComboBox
         '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(124, 30)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(286, 21)
-        Me.ComboBox1.TabIndex = 17
+        Me.fumoComboBox.FormattingEnabled = True
+        Me.fumoComboBox.Location = New System.Drawing.Point(124, 30)
+        Me.fumoComboBox.Name = "fumoComboBox"
+        Me.fumoComboBox.Size = New System.Drawing.Size(515, 21)
+        Me.fumoComboBox.TabIndex = 17
         '
-        'TextBox1
+        'TotalHarga
         '
-        Me.TextBox1.Enabled = False
-        Me.TextBox1.Location = New System.Drawing.Point(124, 100)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(286, 20)
-        Me.TextBox1.TabIndex = 19
+        Me.TotalHarga.Enabled = False
+        Me.TotalHarga.Location = New System.Drawing.Point(124, 100)
+        Me.TotalHarga.Name = "TotalHarga"
+        Me.TotalHarga.ReadOnly = True
+        Me.TotalHarga.Size = New System.Drawing.Size(515, 20)
+        Me.TotalHarga.TabIndex = 19
         '
         'Label3
         '
@@ -149,12 +150,13 @@ Partial Class TransaksiFumo
         Me.Label3.TabIndex = 18
         Me.Label3.Text = "Total Harga"
         '
-        'TextBox2
+        'TotalHargaTampil
         '
-        Me.TextBox2.Location = New System.Drawing.Point(124, 438)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(286, 20)
-        Me.TextBox2.TabIndex = 23
+        Me.TotalHargaTampil.Location = New System.Drawing.Point(124, 438)
+        Me.TotalHargaTampil.Name = "TotalHargaTampil"
+        Me.TotalHargaTampil.ReadOnly = True
+        Me.TotalHargaTampil.Size = New System.Drawing.Size(515, 20)
+        Me.TotalHargaTampil.TabIndex = 23
         '
         'Label4
         '
@@ -170,7 +172,7 @@ Partial Class TransaksiFumo
         '
         Me.TextBox3.Location = New System.Drawing.Point(124, 464)
         Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(286, 20)
+        Me.TextBox3.Size = New System.Drawing.Size(515, 20)
         Me.TextBox3.TabIndex = 21
         '
         'Label5
@@ -183,13 +185,14 @@ Partial Class TransaksiFumo
         Me.Label5.TabIndex = 20
         Me.Label5.Text = "Bayar"
         '
-        'TextBox4
+        'Kembalian
         '
-        Me.TextBox4.Enabled = False
-        Me.TextBox4.Location = New System.Drawing.Point(124, 490)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(286, 20)
-        Me.TextBox4.TabIndex = 25
+        Me.Kembalian.Enabled = False
+        Me.Kembalian.Location = New System.Drawing.Point(124, 490)
+        Me.Kembalian.Name = "Kembalian"
+        Me.Kembalian.ReadOnly = True
+        Me.Kembalian.Size = New System.Drawing.Size(515, 20)
+        Me.Kembalian.TabIndex = 25
         '
         'Label6
         '
@@ -201,45 +204,45 @@ Partial Class TransaksiFumo
         Me.Label6.TabIndex = 24
         Me.Label6.Text = "Kembalian"
         '
-        'Button4
+        'BtnCetak
         '
-        Me.Button4.Location = New System.Drawing.Point(294, 516)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(115, 39)
-        Me.Button4.TabIndex = 26
-        Me.Button4.Text = "Cetak"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.BtnCetak.Location = New System.Drawing.Point(124, 516)
+        Me.BtnCetak.Name = "BtnCetak"
+        Me.BtnCetak.Size = New System.Drawing.Size(115, 39)
+        Me.BtnCetak.TabIndex = 26
+        Me.BtnCetak.Text = "Cetak"
+        Me.BtnCetak.UseVisualStyleBackColor = True
         '
-        'Button5
+        'BtnBersih
         '
-        Me.Button5.Location = New System.Drawing.Point(333, 132)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(76, 27)
-        Me.Button5.TabIndex = 27
-        Me.Button5.Text = "Bersih"
-        Me.Button5.UseVisualStyleBackColor = True
+        Me.BtnBersih.Location = New System.Drawing.Point(369, 132)
+        Me.BtnBersih.Name = "BtnBersih"
+        Me.BtnBersih.Size = New System.Drawing.Size(76, 27)
+        Me.BtnBersih.TabIndex = 27
+        Me.BtnBersih.Text = "Bersih"
+        Me.BtnBersih.UseVisualStyleBackColor = True
         '
         'TransaksiFumo
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(423, 563)
-        Me.Controls.Add(Me.Button5)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.TextBox4)
+        Me.ClientSize = New System.Drawing.Size(651, 563)
+        Me.Controls.Add(Me.BtnBersih)
+        Me.Controls.Add(Me.BtnCetak)
+        Me.Controls.Add(Me.Kembalian)
         Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TotalHargaTampil)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.TextBox3)
         Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TotalHarga)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.ComboBox1)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.DataGridView1)
-        Me.Controls.Add(Me.InHarga)
+        Me.Controls.Add(Me.fumoComboBox)
+        Me.Controls.Add(Me.BtnHapus)
+        Me.Controls.Add(Me.BtnEdit)
+        Me.Controls.Add(Me.BtnTambah)
+        Me.Controls.Add(Me.TableTransaksi)
+        Me.Controls.Add(Me.InJumlah)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.MenuStrip1)
@@ -249,7 +252,7 @@ Partial Class TransaksiFumo
         Me.TopMost = True
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TableTransaksi, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -257,22 +260,22 @@ Partial Class TransaksiFumo
 
     Friend WithEvents MenuStrip1 As MenuStrip
     Friend WithEvents KembaliToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button1 As Button
-    Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents InHarga As TextBox
+    Friend WithEvents BtnHapus As Button
+    Friend WithEvents BtnEdit As Button
+    Friend WithEvents BtnTambah As Button
+    Friend WithEvents TableTransaksi As DataGridView
+    Friend WithEvents InJumlah As TextBox
     Friend WithEvents Label2 As Label
     Friend WithEvents Label1 As Label
-    Friend WithEvents ComboBox1 As ComboBox
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents fumoComboBox As ComboBox
+    Friend WithEvents TotalHarga As TextBox
     Friend WithEvents Label3 As Label
-    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TotalHargaTampil As TextBox
     Friend WithEvents Label4 As Label
     Friend WithEvents TextBox3 As TextBox
     Friend WithEvents Label5 As Label
-    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Kembalian As TextBox
     Friend WithEvents Label6 As Label
-    Friend WithEvents Button4 As Button
-    Friend WithEvents Button5 As Button
+    Friend WithEvents BtnCetak As Button
+    Friend WithEvents BtnBersih As Button
 End Class
--- FumoStore/TransaksiFumo.vb
+++ FumoStore/TransaksiFumo.vb
@@ -1,4 +1,11 @@
-﻿Public Class TransaksiFumo
+﻿Imports iText.StyledXmlParser.Jsoup.Select.Evaluator
+
+Public Class TransaksiFumo
+    Dim m_fumos As List(Of Fumo) = New List(Of Fumo)
+    Dim m_table As DataTable = New DataTable
+    Dim m_index = 1
+    Dim NO = -1
+
     Private Sub Kembali()
         Me.Dispose()
     End Sub
@@ -10,5 +17,142 @@
 
     Private Sub KembaliToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KembaliToolStripMenuItem.Click
         Kembali()
+    End Sub
+
+    Private Sub Bersih()
+        fumoComboBox.SelectedIndex = -1
+        InJumlah.Text = ""
+        TotalHarga.Text = ""
+    End Sub
+
+    Private Sub ModeTambah()
+        BtnEdit.Enabled = False
+        BtnHapus.Enabled = False
+        BtnTambah.Enabled = True
+        NO = -1
+    End Sub
+
+    Private Sub ModeEditDanHapus()
+        BtnEdit.Enabled = True
+        BtnHapus.Enabled = True
+        BtnTambah.Enabled = False
+    End Sub
+
+    Private Sub TransaksiFumo_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim fumoRaw = FumoRepository.All()
+        For Each row As DataRow In fumoRaw.Rows
+            Dim fumo = New Fumo(row("id"), row("nama"), row("rawharga"))
+            fumoComboBox.Items.Add(fumo.m_Nama & " - " & fumo.m_Harga)
+            m_fumos.Add(fumo)
+        Next
+        m_table.Columns.Add("no", GetType(String))
+        m_table.Columns.Add("id", GetType(String))
+        m_table.Columns.Add("nama", GetType(String))
+        m_table.Columns.Add("jumlah", GetType(String))
+        m_table.Columns.Add("rawharga", GetType(Integer))
+        m_table.Columns.Add("rawtotal", GetType(Integer))
+
+        UpdateTampil()
+        ModeTambah()
+    End Sub
+
+    Private Sub UpdateTampil()
+        Dim clone = m_table.Copy()
+        clone.Columns.Add("harga")
+
+        For Each row As DataRow In clone.Rows
+            row("harga") = String.Format(Indonesian.Culture, "{0:C}", row("rawharga"))
+        Next
+        clone.Columns.Add("total")
+        For Each row As DataRow In clone.Rows
+            row("total") = String.Format(Indonesian.Culture, "{0:C}", row("rawharga") * row("rawtotal"))
+        Next
+        clone.Columns.Remove("rawharga")
+        clone.Columns.Remove("rawtotal")
+        TableTransaksi.DataSource = New DataView(clone)
+    End Sub
+
+    Private Sub BtnTambah_Click(sender As Object, e As EventArgs) Handles BtnTambah.Click
+        Dim Index = fumoComboBox.SelectedIndex()
+        If Index < 0 Or Index > m_fumos.Count Then
+            MsgBox("Belum memilih barang")
+            Return
+        End If
+
+        Try
+            Dim fumo = m_fumos(Index)
+            Dim row = m_table.NewRow()
+            row("no") = m_index
+            row("id") = fumo.m_Id
+            row("nama") = fumo.m_Nama
+            row("jumlah") = InJumlah.Text
+            row("rawharga") = fumo.m_Harga
+            row("rawtotal") = fumo.m_Harga * Val(InJumlah.Text)
+            m_table.Rows.Add(row)
+
+            UpdateTampil()
+            m_index += 1
+        Catch ex As Exception
+            MsgBox("Harga tidak berbentuk angka")
+        End Try
+    End Sub
+
+    Private Sub InJumlah_TextChanged(sender As Object, e As EventArgs) Handles InJumlah.TextChanged
+        Dim Index = fumoComboBox.SelectedIndex()
+        If Index < 0 Or Index > m_fumos.Count Then
+            Return
+        End If
+        Try
+            Dim fumo = m_fumos(Index)
+            TotalHarga.Text = fumo.m_Harga * Val(InJumlah.Text)
+        Catch ex As Exception
+        End Try
+
+    End Sub
+
+    Private Sub ClickCell(sender As Object, e As DataGridViewCellEventArgs) Handles TableTransaksi.CellClick
+        If e.RowIndex >= 0 Then
+            Try
+                Dim selectedRow As DataGridViewRow = TableTransaksi.Rows(e.RowIndex)
+                Dim idValue As Integer = Convert.ToInt32(selectedRow.Cells("id").Value)
+                Dim fumo As Fumo = FumoRepository.Find(idValue)
+                NO = e.RowIndex
+                Dim index = 0
+                For Each fumo In m_fumos
+                    If String.Compare(fumo.m_Id, idValue) = 0 Then
+                        Exit For
+                    End If
+                    index += 1
+                Next
+                fumoComboBox.SelectedIndex = index
+                InJumlah.Text = selectedRow.Cells("jumlah").Value
+                ModeEditDanHapus()
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+        End If
+    End Sub
+
+    Private Sub BtnBersih_Click(sender As Object, e As EventArgs) Handles BtnBersih.Click
+        ModeTambah()
+        Bersih()
+    End Sub
+
+    Private Sub BtnEdit_Click(sender As Object, e As EventArgs) Handles BtnEdit.Click
+        Try
+            Dim row = m_table.Rows(NO)
+            Dim idValue As Integer = Convert.ToInt32(row("id").Value)
+            Dim fumo As Fumo = FumoRepository.Find(idValue)
+            row("jumlah") = InJumlah.Text
+            row("total") = Val(InJumlah.Text) * fumo.m_Harga
+            UpdateTampil()
+        Catch ex As Exception
+            MessageBox.Show("Data tidak valid")
+        End Try
+    End Sub
+
+    Private Sub BtnHapus_Click(sender As Object, e As EventArgs) Handles BtnHapus.Click
+        m_table.Columns.RemoveAt(NO)
+        UpdateTampil()
     End Sub
 End Class
