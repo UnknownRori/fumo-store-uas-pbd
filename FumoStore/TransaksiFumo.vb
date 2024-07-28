82b9b9442205d3b8f9cb7b78faf42fb83df101bc
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2024-07-28 18:13:34 +0700

    add kwitansi

--- FumoStore/TransaksiFumo.vb
+++ FumoStore/TransaksiFumo.vb
@@ -1,4 +1,7 @@
-﻿Imports iText.StyledXmlParser.Jsoup.Select.Evaluator
+﻿Imports iText.Kernel.Pdf
+Imports iText.Layout
+Imports iText.Layout.Element
+Imports iText.Layout.Properties
 
 Public Class TransaksiFumo
     Dim m_fumos As List(Of Fumo) = New List(Of Fumo)
@@ -176,5 +179,106 @@ Public Class TransaksiFumo
             TotalHarga.Text = String.Format(Indonesian.Culture, "{0:C}", fumo.m_Harga * Val(InJumlah.Text))
         Catch ex As Exception
         End Try
+    End Sub
+
+    Private Sub BtnCetak_Click(sender As Object, e As EventArgs) Handles BtnCetak.Click
+        Dim saveFileDialog As New SaveFileDialog()
+        saveFileDialog.DefaultExt = "pdf"
+        saveFileDialog.Filter = "Document files (*.pdf)|*.pdf"
+
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+
+        Else
+            MsgBox("Data batal disimpan!")
+            Return
+        End If
+
+        Dim filePath As String = saveFileDialog.FileName
+        MsgBox("Proses")
+
+        total_harga = 0
+        Dim data = m_table.Copy()
+        data.Columns.Add("harga")
+
+        For Each row As DataRow In data.Rows
+            row("harga") = String.Format(Indonesian.Culture, "{0:C}", row("rawharga"))
+        Next
+        data.Columns.Add("total")
+        For Each row As DataRow In data.Rows
+            total_harga += Val(row("rawtotal"))
+            row("total") = String.Format(Indonesian.Culture, "{0:C}", Val(row("rawtotal")))
+        Next
+        data.Columns.Remove("rawharga")
+        data.Columns.Remove("id")
+        data.Columns.Remove("rawtotal")
+        Try
+            Using writer As New PdfWriter(filePath)
+                Using pdf As New PdfDocument(writer)
+                    Using document As New Document(pdf)
+                        Dim title As New Paragraph("KWITANSI PEMBELIAN")
+                        title.SetFontSize(24).SetBold()
+                        title.SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER)
+                        document.Add(title)
+
+                        Dim subtitle As New Paragraph("TOKO FUMO")
+                        subtitle.SetFontSize(18).SetBold()
+                        subtitle.SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER)
+                        document.Add(subtitle)
+
+                        document.Add(New Paragraph(""))
+                        document.Add(New Paragraph(""))
+                        document.Add(New Paragraph("Tanggal : " & Date.Now))
+                        document.Add(New Paragraph(""))
+
+                        Dim table As New Table(data.Columns.Count)
+                        table.SetWidth(iText.Layout.Properties.UnitValue.CreatePercentValue(100))
+
+                        ' Add headers to the table
+                        For Each column As DataColumn In data.Columns
+                            table.AddHeaderCell(New Cell().Add(New Paragraph(column.ColumnName).SetBold().SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER)))
+                        Next
+
+                        ' Add data rows to the table
+                        For Each row As DataRow In data.Rows
+                            For Each column As DataColumn In data.Columns
+                                Dim par = New Paragraph(row(column).ToString())
+                                table.AddCell(New Cell().Add(par))
+                            Next
+                        Next
+
+                        table.AddCell(New Cell(0, 4).Add(New Paragraph("Total").SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER)))
+                        Dim formattedNumber As String = String.Format(Indonesian.Culture, "{0:C}", total_harga)
+                        table.AddCell(New Cell().Add(New Paragraph(formattedNumber)))
+                        table.AddCell(New Cell(0, 4).Add(New Paragraph("Bayar").SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER)))
+                        Dim formattedNumber2 As String = String.Format(Indonesian.Culture, "{0:C}", Val(InBayar.Text))
+                        table.AddCell(New Cell().Add(New Paragraph(formattedNumber2)))
+                        table.AddCell(New Cell(0, 4).Add(New Paragraph("Kembalian").SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER)))
+                        Dim formattedNumber3 As String = String.Format(Indonesian.Culture, "{0:C}", Val(InBayar.Text) - total_harga)
+                        table.AddCell(New Cell().Add(New Paragraph(formattedNumber3)))
+
+                        ' Add the table to the document
+                        document.Add(table)
+
+                        document.Add(New Paragraph(""))
+                        Dim dumb = New Paragraph("").AddTabStops(New Element.TabStop(50))
+                        dumb.Add(New Tab()).Add("   Kasir")
+                        document.Add(dumb)
+
+                        document.Add(New Paragraph(""))
+                        document.Add(New Paragraph(""))
+                        document.Add(New Paragraph(""))
+                        document.Add(New Paragraph(""))
+                        document.Add(New Paragraph(""))
+                        document.Add(New Paragraph(""))
+                        Dim tab = New Paragraph("-----------------------------------------").AddTabStops(New Element.TabStop(50))
+                        document.Add(tab)
+                    End Using
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox("Data gagal di cetak!")
+            MsgBox(ex.Message)
+        End Try
+        MsgBox("Data berhasil disimpan!")
     End Sub
 End Class
